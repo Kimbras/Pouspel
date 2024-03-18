15a2b2c2dc5973a355234d58bed81c0078e77c6e
Kimbras <kimbras2002@gmail.com>
2024-03-18 17:49:54 +0100

    audio, for each toegevoegd, code netjes gemaakt

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,38 +1,45 @@
-console.log("Hello world!");
 document.body.style.background = "#EFD174";
 
+//alle const
+
+    const etenLevel = document.querySelector("#etenLevel");
+    const eetKnop = document.querySelector("#eetKnop");
+
+    const energieLevel = document.querySelector("#energieLevel");
+    const energieKnop = document.querySelector("#energieKnop");
+
+    const zeepKnop = document.querySelector("#zeep");
+
+    const pouSlapen = document.querySelector("#pou");
+    const slaapKnop = document.querySelector("#slaapKnop");
+
+    const andereKnoppen = document.querySelectorAll(".andereKnoppen");
+
+    const kledingKnop = document.querySelector("#kledingKnop");
+    const kledingKnopTerug = document.querySelector("#terugKnop").classList;
+    const kledingKnopVerder = document.querySelector("#verderKnop").classList;
+
 //etenknop verhogen en verlagen met timer
-const etenLevel = document.querySelector("#etenLevel");
-const eetKnop = document.querySelector("#eetKnop");
 
 function verlaagEetLevel(){
     document.querySelector("#etenLevel").src = 'img/levels/eten.svg';  
 }
-
 function verhoogEetLevel(){
     document.querySelector("#etenLevel").src = 'img/levels/etengroen.svg';
     setInterval(verlaagEetLevel, 8000); 
 }
-eetKnop.addEventListener("click", verhoogEetLevel);
 
 //energieknop verhogen en verlagen met timer
-const energieLevel = document.querySelector("#energieLevel");
-const energieKnop = document.querySelector("#energieKnop");
-
 function verlaagEnergieLevel(){
     document.querySelector("#energieLevel").src = 'img/levels/energie.svg';   
 }
-
 function verhoogEnergieLevel(){
     document.querySelector("#energieLevel").src = 'img/levels/energiegroen.svg';
     setInterval(verlaagEnergieLevel, 5000);
 }
 
-energieKnop.addEventListener("click", verhoogEnergieLevel);
-
 // zeep
 // erik heeft geholpen
-const zeepKnop = document.querySelector("#zeep");
 let pouArray = ['schoon.png','vies.png','viezer.png']
 let statusPou = 0;
 document.querySelector("#pou").src = 'img/karacters/'+pouArray[statusPou];
@@ -41,11 +48,9 @@ function pouSchoon(){
     statusPou = statusPou - 1;
     if (statusPou < 0){
         statusPou = 0;
-        document.body.style.background = "#a4f558";
-
-
-        
+        document.body.style.background = "#a4f558";       
     }
+
     else if (statusPou > 2){
         statusPou = 2;
         document.body.style.background = "#64a12a";
@@ -77,12 +82,8 @@ function pouUpdaten(){
 }
 
 setInterval(viesWorden, 12000);
- 
-zeepKnop.addEventListener("click", pouSchoon);
 
 //slapen
-const pouSlapen = document.querySelector("#pou");
-const slaapKnop = document.querySelector("#slaapKnop");
 
 function KlaarmetSlapen(){
     document.querySelector("#pou").src = 'img/karacters/schoon.png'; 
@@ -95,57 +96,77 @@ function GaanSlapen(){
     document.body.style.background = "#0a1d7a";
 }
 
-
-slaapKnop.addEventListener("click", GaanSlapen);
-
 // geluid toevoegen
 // https://pixabay.com/nl/sound-effects/search/klink/
 
 let audioKledingkast = new Audio("img/audio/klikgeluidKledingkast.mp3");
 let audioSlapen = new Audio("img/audio/slapen.mp3");
 
-function geluid (knop){
-    knop.play;
-    console.log("check")
+function geluid(audiofragment){
+    audiofragment.play();
 }
-
-kledingKnop.addEventListener("click", geluid(audioKledingkast));
-slaapKnop.addEventListener("click", geluid(audioSlapen));
 
 // kledingkast
 //terug en verder knop in beeld
 
-    const kledingKnop = document.querySelector("#kledingKnop");
-    const kledingKnopTerug = document.querySelector("#terugKnop").classList;
-    const kledingKnopVerder = document.querySelector("#verderKnop").classList;
-
     kledingKnopTerug.add("navigatieVerbergen");
     kledingKnopVerder.add("navigatieVerbergen");
-    
+
     function kledingKnoppenTonen(){
         kledingKnopTerug.add("navigatieShow");
         kledingKnopVerder.add("navigatieShow");
     }
     
-     // function kledingKnoppenVerbergen(){
-   //     kledingKnopTerug.classList.remove("navigatieVerbergen");
-   //     kledingKnopVerder.classList.remove("navigatieVerbergen");
-   // }
 
-   kledingKnop.addEventListener("click", kledingKnoppenTonen);
-   //  andereKnoppen.addEventListener("click", kledingKnoppenVerbergen);
+    function kledingKnoppenVerbergen(){
+    kledingKnopTerug.remove("navigatieShow");
+    kledingKnopVerder.remove("navigatieShow");
+    kledingKnopTerug.add("navigatieVerbergen");
+    kledingKnopVerder.add("navigatieVerbergen");
+   }
 
-   
- 
+   andereKnoppen.forEach((knop) => knop.addEventListener("click", kledingKnoppenVerbergen))
+  
  // math random kleding toevoegen
  
  let outfitsArray = ['een.png', 'twee.png', 'drie.png', 'vier.png', 'vijf.png'];
- let imgElement = document.querySelector(".Wakker");
+ let imgStaat = document.querySelector(".Wakker");
  
  function kledingRandomAanpassen() {
      let randomIndex = Math.floor(Math.random() * 4);
      let randomOutfit = outfitsArray[randomIndex];
-     imgElement.src = 'img/karacters/' + randomOutfit;
+     imgStaat.src = 'img/karacters/' + randomOutfit;
  }
- console.log(kledingRandomAanpassen);
- //kledingKnopVerder.addEventListener("click", kledingRandomAanpassen);
+ 
+ 
+//  let outfitsArray = ['een.png', 'twee.png', 'drie.png', 'vier.png', 'vijf.png'];
+//  let imgElement = document.querySelector(".Wakker");
+
+ 
+//  function kledingVerder(){
+//     outfitsArray = outfitsArray + 1;  
+//     imgElement.src = 'img/karacters/' + randomOutfit;    
+//     }
+
+//     kledingKnopVerder.addEventListener("click", kledingVerder);
+
+//     function kledingRandomAanpassen() {
+//      let randomIndex = Math.floor(Math.random() * 4);
+//      let randomOutfit = outfitsArray[randomIndex];
+//      imgElement.src = 'img/karacters/' + randomOutfit;
+//  }
+
+//addEventListener
+eetKnop.addEventListener("click", verhoogEetLevel);
+energieKnop.addEventListener("click", verhoogEnergieLevel);
+zeepKnop.addEventListener("click", pouSchoon);
+slaapKnop.addEventListener("click", GaanSlapen);
+kledingKnop.addEventListener("click", () => {
+    kledingKnoppenTonen();
+    geluid(audioKledingkast);
+});
+
+slaapKnop.addEventListener("click", () => {
+    geluid(audioSlapen)
+});
+
